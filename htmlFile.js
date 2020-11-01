5d60575c676c23954966170b812bf2ad756a5c82
sasoara <b.sarah2@hotmail.com>
2020-11-01 15:30:33 +0100

    efficient provision of HTML

--- htmlFile.js
+++ htmlFile.js
@@ -7,26 +7,31 @@ const fs = require('fs/promises');
 const host = 'localhost';
 // Defines the port to which the server is bound
 const port = 8000;
+// The content of this file should be output
+let indexFile;
+// Creates the server
+const server = http.createServer(requestListener);
+
 // Handle the request and give the response
 const requestListener = function (req, res) {
     // Variable __dirname shows the absolute path
     fs.readFile(__dirname + "/index.html")
         // Return the HTML page as soon as it has loads:
         .then(contents => {
+            // The file is loaded before the server runs.
+            indexFile = contents;
+            // Bind the server to a network address and listen.
+            server.listen(port, host, () => {
+                console.log(`Server is running on http://${host}:${port}`);
+            });
             res.setHeader("Content-Type", "text/html");
             res.writeHead(200);
-            res.end(contents);
+            res.end(indexFile);
         })
         .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
+            // If the file cannot be loaded, capture the error and report it to the console.
+            console.error(`Could not read index.html file: ${err}`);
+            process.exit(1);
         });
 };
-// Creates the server
-const server = http.createServer(requestListener);
-// Bind the server to a network address and listen.
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
 
